87f174c3a34151c939172e7a195c0566922a2f13
Efealtan0909 <60935052+Efealtan0909@users.noreply.github.com>
2020-06-23 01:27:58 +0300

    Uploading Files For Startuğ

---
+++ VDedector/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VDedector
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press A Character That Is Not V");
+            string Key = Convert.ToString(Console.ReadKey())    ;
+            if(Key == "V" || Key == "v")
+            {
+                Console.WriteLine("You Are Dumb");
+            } else
+            {
+                Console.WriteLine("Thx For Understanding This Rule");
+            }
+        }
+    }
+}
