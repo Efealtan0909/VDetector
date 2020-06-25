77b5f4c522d2a59459ff32196b287f17bf4cb0c1
Efealtan0909 <60935052+Efealtan0909@users.noreply.github.com>
2020-06-25 18:23:47 +0300

    New Version

--- VDedector/Program.cs
+++ VDedector/Program.cs
@@ -6,15 +6,19 @@ namespace VDedector
     {
         static void Main()
         {
-            Console.WriteLine("Press A Character That Is Not V");
-            string Key = Convert.ToString(Console.ReadKey())    ;
-            if(Key == "V" || Key == "v")
+            Console.WriteLine("Type A Thing That Doesnt Contain V");
+            string Typed_Word = Console.ReadLine();
+            int start = 0;
+            int length = Typed_Word.Length;
+            string h = Typed_Word.Substring(start, length);
+            if(h.Contains("V") == true || h.Contains("v") == true)
             {
                 Console.WriteLine("You Are Dumb");
             } else
             {
                 Console.WriteLine("Thx For Understanding This Rule");
             }
+            Console.ReadKey();
         }
     }
 }
